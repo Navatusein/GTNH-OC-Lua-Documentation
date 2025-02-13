87c101fe32fc5107bed85e70619c787268b9b68b
Navatusein <boghdan.kutsulima@gmail.com>
2025-02-13 14:41:35 +0200

    Update component lib

--- lua/libs/component.lua
+++ lua/libs/component.lua
@@ -55,7 +55,7 @@ function component.methods(address) end
 ---Note that proxies will always have at least two fields, type with the component's type name, and address with the component's address.
 ---@generic T
 ---@param address string # The full address of the component.
----@param type `T`
+---@param type? `T`
 ---@return T
 function component.proxy(address, type) end
 
